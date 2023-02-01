ca1b5e23598fff441957e180c96e3bbd69fac4b6
0xpwnstar <chatna717@gmail.com>
2023-02-01 18:35:55 +0530

    Connected Mysql

--- web/router/api/authentication.js
+++ web/router/api/authentication.js
@@ -1,5 +1,23 @@
+const mysql = require('mysql');
+var connection = mysql.connection({
+    host : 'nodjs-base.cluster-cjskwdik5gvo.ap-south-1.rds.amazonaws.com',
+    user : 'admin',
+    password : 'c7508TAN!',
+    database : 'insta_clone'
+})
+
+const query = () => {
+    connection.connect();
+    connection.query('SELECT * from users',(err,results) => {
+        if (err) console.log(err);
+        console.log(results)
+        return results
+    })
+}
+
 const user = (req,res) => {
-    res.send("Chaitu")
+    
+    res.send(query())
 }
 
 module.exports = user;
