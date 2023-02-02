57e694c8506f7ecc68c9e7204b452d98b26c75d6
0xpwnstar <chatna717@gmail.com>
2023-02-02 22:00:28 +0530

    Added hashing for passwords 3

--- web/router/api/signup.js
+++ web/router/api/signup.js
@@ -1,22 +1,23 @@
 const bodyParser = require('body-parser')
 const mysql = require('mysql');
-const sha256 = require('sha256');
+const crypto = require('crypto')
 const connection = mysql.createConnection({
     host : 'nodjs-base.cluster-cjskwdik5gvo.ap-south-1.rds.amazonaws.com',
     user : 'admin',
     password : 'c7508TAN!',
     database : 'insta_clone'
 })
-const isEmailExist = (firstname, lastname, password, salt, email) => {
+const isEmailExist = (firstname, lastname, password,  email) => {
     connection.query('SELECT COUNT(*) AS TEMAIL FROM users WHERE email=?',[email],(err,results) => {
         if (err) console.log("Error in Email Validation");
         console.log(results)
-        results[0].TEMAIL == 0 ? register(firstname, lastname, password, salt, email) : false
+        results[0].TEMAIL == 0 ? register(firstname, lastname, password, email) : false
     })
 }
 
-const register = (firstname, lastname, password, salt, email) => {
-    password = sha256(password+salt);
+const register = (firstname, lastname, password, email) => {
+    salt = crypto.randomBytes(12).toString('hex').slice(0,12);
+    password = crypto.createHmac('sha256',salt).update(password).digest('hex');
     console.log("registerting")
     connection.query('Insert into users SET ?',{firstname,lastname,password,salt,email},(err,results) => {
         if (err) console.log("Error in user registeration");
@@ -29,7 +30,7 @@ exports.jsonParser = bodyParser.json()
 
 exports.signup = (req,res) => {
     body = req.body
-    isEmailExist(body.firstname,body.lastname,body.password,body.salt, body.email)
+    isEmailExist(body.firstname,body.lastname,body.password, body.email)
     res.send("Registered")
 }
 
