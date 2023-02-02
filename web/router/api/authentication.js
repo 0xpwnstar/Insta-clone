89884ec1bb7a4ca01354ea55278819d0aadef410
0xpwnstar <chatna717@gmail.com>
2023-02-02 20:00:05 +0530

    Patching queries 9

--- web/router/api/authentication.js
+++ web/router/api/authentication.js
@@ -1,21 +1,26 @@
 const mysql = require('mysql');
-var connection = mysql.createConnection({
+const util = require('util');
+const config = {
     host : 'nodjs-base.cluster-cjskwdik5gvo.ap-south-1.rds.amazonaws.com',
     user : 'admin',
     password : 'c7508TAN!',
     database : 'insta_clone'
-})
-const query = () => {
-    return new Promise((resolve, reject) => {
-        connection.query('SELECT * from users',(err,results) => {
-            if (err) console.log(err)
-            return resolve(results)
-        })
-    })
+}
+const conn = mysql.createConnection(config)
+const query = util.promisify(conn.query).bind(conn)
+async users = () => {
+    var rows = []
+    try {
+        const row = await query('SELECT * from users')
+        }
+        finally {
+            conn.end();
+            return rows
+        }
 }
 
 const user = (req,res) => {
-    answer = await query()
+    answer = users(),then(value => value)
     console.log(answer)
     res.send({message: answer})
 }
