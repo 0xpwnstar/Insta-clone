e8fa6853d3573afb0bb1c99e1b096c4401f03854
0xpwnstar <chatna717@gmail.com>
2023-02-03 21:07:57 +0530

    patched api 2

--- web/router/api/auth/index.js
+++ web/router/api/auth/index.js
@@ -1,8 +1,10 @@
 const users = require('./authentication');
 const { jsonParser, signup } = require('./signup');
+const { signin } = require('./signin');
 const express = require('express')
 const router = express.Router();
 
 router.post('/signup',jsonParser,signup)
 router.get('/users', users);
+router.post('/signin',signin);
 module.exports = router;
---
+++ web/router/api/auth/signin.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql');
+const crypto = require('crypto')
+const connection = mysql.createConnection({
+    host : 'nodjs-base.cluster-cjskwdik5gvo.ap-south-1.rds.amazonaws.com',
+    user : 'admin',
+    password : 'c7508TAN!',
+    database : 'insta_clone'
+})
+const isEmailExist = (email, password) => {
+    connection.query('SELECT COUNT(*) AS TEMAIL FROM users WHERE email=?',[email],(err,results) => {
+        if (err) console.log("Error in Email Validation");
+        console.log(results)
+        results[0].TEMAIL == 0 ? signin(firstname, lastname, password, email) : false
+    })
+}
+
+const signin = (email, password) => {
+    salt = connection.query('SELECT salt FROM users WHERE email=?',[email],(err,results) => { if (err) console.log(err) else {console.log(results);return results[0]}})
+    password = crypto.createHmac('sha256',salt).update(password).digest('hex');
+    console.log(password)
+}
+
+
+
+exports.signin = (req,res) => {
+    body = req.body
+    isEmailExist(body.email,body.password)
+    res.send("Login")
+}
