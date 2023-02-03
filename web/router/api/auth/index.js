ff4c2bccb761e2e124f8c28f400bb142d02bb9c7
0xpwnstar <chatna717@gmail.com>
2023-02-03 21:13:18 +0530

    patched api 5

--- web/router/api/auth/index.js
+++ web/router/api/auth/index.js
@@ -6,5 +6,5 @@ const router = express.Router();
 
 router.post('/signup',jsonParser,signup)
 router.get('/users', users);
-router.post('/signin',signin);
+router.post('/signin',jsonParser,signin);
 module.exports = router;
