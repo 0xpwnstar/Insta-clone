6fc4abae6226c98232a3258f2ab7b57691bd230e
0xpwnstar <chatna717@gmail.com>
2023-02-03 22:51:52 +0530

    patched api 19

--- web/router/api/auth/signin.js
+++ web/router/api/auth/signin.js
@@ -30,23 +30,17 @@ hashedPassword = () => {
 }
 
 
-const signInIfEmailExists = (email, password) => {
-    return Promise((resolve,reject) => {
-        connection.query('SELECT COUNT(*) AS TEMAIL FROM users WHERE email=?',[email],(err,results) => {
-            if (err) {
-                return reject(err)
-            }
-            resolve(results)
-        })
-    })
-}
+
+
 
 
 
 
 exports.signin = async (req,res) => {
     body = req.body
-    if (await signInIfEmailExists(body.email,body.password)) {
+    if (connection.query('SELECT COUNT(*) AS TEMAIL FROM users WHERE email=?',[email],(err,results) => {
+        if (err) {return 0} return 1
+    })) {
         const salt_ = 0;
         const hashedPassword_ = 0;
         console.log("here")
