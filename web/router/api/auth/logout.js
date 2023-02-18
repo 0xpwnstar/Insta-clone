af86126514d7e378f134a90afb5cb0f85639cbe9
0xpwnstar <chatna717@gmail.com>
2023-02-18 20:59:33 +0530

    Sat push 4

--- web/router/api/auth/logout.js
+++ web/router/api/auth/logout.js
@@ -1,25 +1,54 @@
 const { JSONRPCClient } = require("json-rpc-2.0");
+const axios = require('axios');
 // JSONRPCClient needs to know how to send a JSON-RPC request.
 // Tell it by passing a function to its constructor. The function must take a JSON-RPC request and send it.
-const client = new JSONRPCClient((jsonRPCRequest) =>
-  fetch("http://localhost:3001/json-rpc", {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify(jsonRPCRequest),
-  }).then((response) => {
+const fetch = async (jsonRPCRequest) =>  {
+  console.log(jsonRPCRequest)
+  try {
+    const response = await axios.post("http://localhost:3001/json-rpc",jsonRPCRequest )
+
     if (response.status === 200) {
       // Use client.receive when you received a JSON-RPC response.
-      return response
-        .json()
-        .then((jsonRPCResponse) => client.receive(jsonRPCResponse));
+      return client.receive(response.data);
     } else if (jsonRPCRequest.id !== undefined) {
       return Promise.reject(new Error(response.statusText));
     }
-  }).catch((err) => {
-      return Promise.reject(new Error(err));   
-  })
+  }  catch (err) {
+    console.log(err);
+  }
+
+  // .then((response) => {
+  // if (response.status === 200) {
+  //   // Use client.receive when you received a JSON-RPC response.
+  //   console.log(response)
+  //   return response
+  //     .json()
+  //     .then((jsonRPCResponse) => client.receive(jsonRPCResponse));
+  // } else if (jsonRPCRequest.id !== undefined) {
+  //   return Promise.reject(new Error(response.statusText));
+  // }
+// })}
+}
+const client = new JSONRPCClient((jsonRPCRequest) =>
+  // fetch("http://localhost:3001/json-rpc", {
+  //   method: "POST",
+  //   headers: {
+  //     "content-type": "application/json",
+  //   },
+  //   body: JSON.stringify(jsonRPCRequest),
+  // }).then((response) => {
+  //   if (response.status === 200) {
+  //     // Use client.receive when you received a JSON-RPC response.
+  //     return response
+  //       .json()
+  //       .then((jsonRPCResponse) => client.receive(jsonRPCResponse));
+  //   } else if (jsonRPCRequest.id !== undefined) {
+  //     return Promise.reject(new Error(response.statusText));
+  //   }
+  // }))
+  fetch(jsonRPCRequest)
+
+
 );
 
 const logout = (req,res) => {
