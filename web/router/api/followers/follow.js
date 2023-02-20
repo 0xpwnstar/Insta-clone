ef1cfcbde0ff0acad683792d7e32b084f3262232
0xpwnstar <chatna717@gmail.com>
2023-02-20 19:33:31 +0530

    added followers patch 1

--- web/router/api/followers/follow.js
+++ web/router/api/followers/follow.js
@@ -10,10 +10,8 @@ exports.follow = (req,res) => {
     body = req.body
     following_uid = body.following_uid
     followed_uid = body.followed_uid
-    var results
-    connection.query('Insert into followers SET ?',{following_uid, followed_uid},(err,res) => {
+    connection.query('Insert into followers SET ?',{following_uid, followed_uid},(err,result) => {
         if (err) console.log("Error in user registeration");
-        results = res
+        res.send(result)
     })
-    res.send(res)
 }
