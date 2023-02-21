5e9474b506ddb0b23e48a085f90fb1f140d6ee6f
0xpwnstar <chatna717@gmail.com>
2023-02-21 11:30:38 +0530

    added following activity 3

--- web/router/api/followers/follow.js
+++ web/router/api/followers/follow.js
@@ -10,7 +10,6 @@ alreadyFollowing = (uid1, uid2) => {
     return new Promise((resolve,reject) => {
         connection.query('SELECT COUNT(*) as TOT FROM followers WHERE followed_uid=? and following_uid=?',[uid2, uid1],(err,results) => {
             if (err) {return reject(0)} else  {
-                console.log(results)
                 return resolve(results[0].TOT)
             }
     })
@@ -19,13 +18,13 @@ alreadyFollowing = (uid1, uid2) => {
 
 exports.follow = async (req,res) => {
     body = req.body
-    uid1 = body.following_uid
-    uid2 = body.followed_uid
-    if (alreadyFollowing(uid1, uid2) == 1) {
+    following_uid = body.following_uid
+    followed_uid = body.followed_uid
+    if (alreadyFollowing(following_uid, uid2) == 1) {
         res.send({"alreadyFollowing":1})
     }
     else {
-        connection.query('Insert into followers SET ?',{uid1, uid2},(err,result) => {
+        connection.query('Insert into followers SET ?',{following_uid, followed_uid},(err,result) => {
         if (err) console.log("Error in user registeration");
             res.send(result)
         })
